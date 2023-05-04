84610ca47cb1309434a6161d7d6722b3a6c89233
TianZeng <jingtian.zeng@globetools.com>
2023-05-04 15:17:28 +0800

    Add websocket client code.

--- sample/02-gateways/src/events/events.gateway.ts
+++ sample/02-gateways/src/events/events.gateway.ts
@@ -20,11 +20,13 @@ export class EventsGateway {
 
   @SubscribeMessage('events')
   findAll(@MessageBody() data: any): Observable<WsResponse<number>> {
+    console.log("events");
     return from([1, 2, 3]).pipe(map(item => ({ event: 'events', data: item })));
   }
-
+  
   @SubscribeMessage('identity')
   async identity(@MessageBody() data: number): Promise<number> {
+    console.log("identity");
     return data;
   }
 }
